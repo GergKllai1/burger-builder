5101c051b7c0102cc6fd23413130857375c45d5f
Greg <gerg.kallai@gmail.com>
2019-03-15 17:52:20 +0100

    Adds  basic folder structure Adds layout container for header Adds aux hoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout'
+
 class App extends Component {
   render() {
     return (
       <div>
-       Hello World
+        <Layout>
+          <p>Hello World</p>
+        </Layout>
       </div>
     );
   }
