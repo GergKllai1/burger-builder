3acf4ce6ed51db66681acb83264f1ca3e1f34f48
Gregory Kallai <gerg.kallai@gmail.com>
2019-08-05 21:29:13 +0200

    Adds basic add and remove ingredient actions

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,14 +25,14 @@ export class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
-    axios
-      .get("https://burger-builder-7d26e.firebaseio.com/ingredients.json")
-      .then(response => {
-        this.setState({ ingredients: response.data });
-      })
-      .catch(error => {
-        this.setState({ error: true });
-      });
+    // axios
+    //   .get("https://burger-builder-7d26e.firebaseio.com/ingredients.json")
+    //   .then(response => {
+    //     this.setState({ ingredients: response.data });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ error: true });
+    //   });
   }
 
   addIngredientHandler = type => {
